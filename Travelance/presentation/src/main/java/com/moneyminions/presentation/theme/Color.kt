f09cb027e651b7d00ee9fd1bf738a7cfc340cae7
황신운 <brandy.hwang@gmail.com>
2023-09-07 15:59:46 +0900

    🎨 style : Color.kt 컬러 값 수정 | 황신운

--- Travelance/presentation/src/main/java/com/moneyminions/presentation/theme/Color.kt
+++ Travelance/presentation/src/main/java/com/moneyminions/presentation/theme/Color.kt
@@ -2,7 +2,6 @@ package com.moneyminions.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -10,3 +9,20 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val PinkDarkest = Color(0xFFFF6666)
+val PinkMiddle = Color(0xFFFF8989)
+val PinkLight = Color(0xFFFCAEAE)
+val PinkLightest = Color(0xFFFFEADD)
+
+val DarkerGray = Color(0xFF3F3F3F)
+val DarkGray = Color(0xFF797979)
+val TextGray = Color(0xFF919191)
+val LightGray = Color(0xFFF6F7F9)
+val CardLightGray = Color(0xFFF9F9F9)
+val GraphGray = Color(0xFFD3D5D8)
+
+val BlueDarkest = Color(0xFF00518B)
+val BlueMiddle = Color(0xFF284F9C)
+val BlueLighter = Color(0xFF4263A3)
+val BlueLightest = Color(0xFF8596B8)
