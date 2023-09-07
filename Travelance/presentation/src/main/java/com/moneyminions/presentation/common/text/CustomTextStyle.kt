fa0e0d8680cb390ad061ace33f07bf1859072332
DESKTOP-DOGVPUB\SSAFY <tjsgh1512@naver.com>
2023-09-07 15:52:04 +0900

    ✨ feat : pretendard Font 추가 | 차선호

---
+++ Travelance/presentation/src/main/java/com/moneyminions/presentation/common/text/CustomTextStyle.kt
@@ -0,0 +1,14 @@
+package com.moneyminions.presentation.common.text
+
+import androidx.compose.ui.text.font.FontFamily
+import com.moneyminions.presentation.R
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+
+object CustomTextStyle{
+
+    val appBarTextStyle = TextStyle(
+        fontFamily = FontFamily(Font(R.font.pretendard_extra_bold))
+    )
+
+}
--- Travelance/presentation/src/main/java/com/moneyminions/presentation/theme/Type.kt
+++ Travelance/presentation/src/main/java/com/moneyminions/presentation/theme/Type.kt
@@ -2,9 +2,23 @@ package com.moneyminions.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.moneyminions.presentation.R
+
+val pretendard = FontFamily(
+    Font(R.font.pretendard_extra_bold, FontWeight.ExtraBold, FontStyle.Normal),
+    Font(R.font.pretendard_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.pretendard_semi_bold, FontWeight.SemiBold, FontStyle.Normal),
+    Font(R.font.pretendard_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.pretendard_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.pretendard_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.pretendard_extra_light, FontWeight.ExtraLight, FontStyle.Normal),
+    Font(R.font.pretendard_thin, FontWeight.Thin, FontStyle.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,21 +28,5 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
