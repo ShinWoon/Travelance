e31587d480984b461e0c14316ac925fe2eb688a8
surimjeon <gds0813@naver.com>
2023-09-07 12:39:27 +0900

    initial commit

---
+++ backend/src/main/java/com/easyone/travelance/TravelanceApplication.java
@@ -0,0 +1,13 @@
+package com.easyone.travelance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelanceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/easyone/travelance/TravelanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyone.travelance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
