7be01af60f962b520c945a053b2da98b5c536a0d
dongHyeok <mimiga1@naver.com>
2023-09-07 17:00:17 +0900

    ✨ feat : Common Button 생성 | 하동혁

---
+++ Travelance/presentation/src/main/java/com/moneyminions/presentation/common/button/Button.kt
@@ -0,0 +1,96 @@
+package com.moneyminions.presentation.common.button
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.moneyminions.presentation.common.text.CustomTextStyle.littleTitleTextStyle
+import com.moneyminions.presentation.common.text.CustomTextStyle.regularTitleTextStyle
+import com.moneyminions.presentation.theme.Gray
+import com.moneyminions.presentation.theme.PinkDarkest
+
+@Composable
+fun MinionPrimaryButton(
+    content: String,
+    modifier: Modifier,
+    isEnabled: MutableState<Boolean> = mutableStateOf(true),
+    onClick: () -> Unit,
+) {
+    Button(
+        modifier = modifier,
+        onClick = { onClick() },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = PinkDarkest,
+            contentColor = White,
+        ),
+        shape = RoundedCornerShape(8.dp),
+        elevation = ButtonDefaults.buttonElevation(4.dp),
+        enabled = isEnabled.value,
+    ) {
+        Text(text = content, style = regularTitleTextStyle)
+    }
+}
+
+@Composable
+fun MinionButtonSet(
+    modifier: Modifier,
+    contentLeft: String,
+    onClickLeft: () -> Unit,
+    contentRight: String,
+    onClickRight: () -> Unit,
+) {
+    Row(
+        modifier = modifier,
+    ) {
+        Button(
+            modifier = modifier.weight(1f),
+            onClick = { onClickLeft() },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = PinkDarkest,
+                contentColor = White,
+            ),
+            shape = RoundedCornerShape(8.dp),
+            elevation = ButtonDefaults.buttonElevation(4.dp),
+        ) {
+            Text(text = contentLeft, style = littleTitleTextStyle)
+        }
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Button(
+            modifier = modifier.weight(1f),
+            onClick = { onClickRight() },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Gray,
+                contentColor = White,
+            ),
+            shape = RoundedCornerShape(8.dp),
+            elevation = ButtonDefaults.buttonElevation(4.dp),
+        ) {
+            Text(text = contentRight, style = littleTitleTextStyle)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MinionButtonSetPreview() {
+    MinionButtonSet(
+        modifier = Modifier.fillMaxWidth(),
+        contentLeft = "추가",
+        contentRight = "취소",
+        onClickLeft = {},
+        onClickRight = {},
+    )
+}
