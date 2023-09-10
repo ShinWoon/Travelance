2f665064e3d499aab3ef81d7c6268d2530def469
theo-no <tjsgh1512@naver.com>
2023-09-11 00:18:33 +0900

    ✨ feat : CardFrame 생성 완료 | 차선호

--- Travelance/presentation/src/main/java/com/moneyminions/presentation/common/CardFrame.kt
+++ Travelance/presentation/src/main/java/com/moneyminions/presentation/common/CardFrame.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -13,9 +14,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import coil.compose.AsyncImage
 import coil.compose.AsyncImagePainter.State.Empty.painter
 import com.moneyminions.presentation.R
 
@@ -34,6 +39,31 @@ fun CardFrame(
     logo: String
 ){
     val imagePainter = painterResource(id = cardFrameList[idx])
+
+    val constraintSet = ConstraintSet{
+        val name = createRefFor("name")
+        val number = createRefFor("number")
+        val company = createRefFor("company")
+        val logo = createRefFor("logo")
+
+        constrain(name){
+            top.linkTo(parent.top, margin = 8.dp)
+            start.linkTo(parent.start, margin = 8.dp)
+        }
+        constrain(number){
+            start.linkTo(parent.start, margin = 8.dp)
+            bottom.linkTo(parent.bottom, margin = 16.dp)
+        }
+        constrain(company){
+            end.linkTo(parent.end, margin = 8.dp)
+            bottom.linkTo(parent.bottom, margin = 16.dp)
+        }
+        constrain(logo){
+            end.linkTo(company.start, margin = 8.dp)
+            bottom.linkTo(company.bottom)
+        }
+    }
+
     Box(
         modifier = Modifier
             .aspectRatio(imagePainter.intrinsicSize.width / imagePainter.intrinsicSize.height)
@@ -53,6 +83,35 @@ fun CardFrame(
                     .aspectRatio(imagePainter.intrinsicSize.width / imagePainter.intrinsicSize.height),
 
                 contentScale = ContentScale.Crop
+            )
+        }
+        ConstraintLayout(
+            constraintSet,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text(
+                text = name,
+                style = CustomTextStyle.pretendardSemiBold16,
+                color = Color.White,
+                modifier = Modifier.layoutId("name")
+            )
+            Text(
+                text = number,
+                style = CustomTextStyle.pretendardBold20,
+                color = Color.White,
+                modifier = Modifier.layoutId("number")
+            )
+            Text(
+                text = company,
+                style = CustomTextStyle.pretendardSemiBold12,
+                color = Color.White,
+                modifier = Modifier.layoutId("company")
+            )
+            AsyncImage(
+                model = logo,
+                contentDescription = null,
+                modifier = Modifier.layoutId("logo")
+                    .size(30.dp)
             )
         }
     }
