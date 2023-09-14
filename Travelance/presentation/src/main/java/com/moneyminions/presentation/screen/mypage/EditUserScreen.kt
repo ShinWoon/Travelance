3499c83372d92f359294e500e3c98776b75a0cb5
DESKTOP-DOGVPUB\SSAFY <tjsgh1512@naver.com>
2023-09-14 17:29:40 +0900

    ✏ modify : 카드 목록 HorizontalPager로 수정 완료 | 차선호

--- Travelance/presentation/src/main/java/com/moneyminions/presentation/screen/mypage/EditUserScreen.kt
+++ Travelance/presentation/src/main/java/com/moneyminions/presentation/screen/mypage/EditUserScreen.kt
@@ -60,21 +60,21 @@ val accountList = listOf(
 )
 val cardList = listOf(
     CardDto(
-        name = "카드 별칭",
+        name = "카드 별칭 1",
         number = "123456789012",
         idx = 0,
         company = "카드사명",
         logo = "https://www.shinhancard.com/pconts/company/images/contents/shc_symbol_ci.png"
     ),
     CardDto(
-        name = "카드 별칭",
+        name = "카드 별칭 2",
         number = "123456789012",
         idx = 1,
         company = "카드사명",
         logo = "https://www.shinhancard.com/pconts/company/images/contents/shc_symbol_ci.png"
     ),
     CardDto(
-        name = "카드 별칭",
+        name = "카드 별칭 3",
         number = "123456789012",
         idx = 2,
         company = "카드사명",
