f4f3b6c25c4613fe0e1671cb40a7be5271e08c69
surimjeon <gds0813@naver.com>
2023-09-18 15:16:28 +0900

    🛠 fix : merge 후 충돌 해결 | 전수림

--- backend/src/main/java/com/easyone/travelance/domain/payment/repository/PaymentRepository.java
+++ backend/src/main/java/com/easyone/travelance/domain/payment/repository/PaymentRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
-    List<Payment> findByRoomId(Long roomId);
+    List<Payment> findByTravelRoomId(Long roomId);
 
     List<Payment> findByTravelRoomIdAndMemberId(Long roomId, Long memberId);
 }
