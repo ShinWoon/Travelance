36b532cda80d3291f832815676ce5def0fed41d6
김제준 <hoilday5303@naver.com>
2023-09-19 11:28:31 +0900

    ✨ feat : 내 전체계좌 조회 구현 | 김제준

--- backend/src/main/java/com/easyone/travelance/domain/account/controller/AccountController.java
+++ backend/src/main/java/com/easyone/travelance/domain/account/controller/AccountController.java
@@ -1,17 +1,37 @@
 package com.easyone.travelance.domain.account.controller;
 
+import com.easyone.travelance.domain.account.service.AccountService;
+import com.easyone.travelance.domain.member.entity.Member;
+import com.easyone.travelance.domain.member.service.MemberService;
+import com.easyone.travelance.global.memberInfo.MemberInfo;
+import com.easyone.travelance.global.memberInfo.MemberInfoDto;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/account")
 public class AccountController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World!";
+    private final MemberService memberService;
+    private final AccountService accountService;
+
+    @Operation(summary = "전체계좌 조회", description = "나의 모든 계좌를 조회하는 메서드 입니다")
+    @PostMapping("/allaccount")
+    public Mono<ResponseEntity<List<Object>>> allAccount(@MemberInfo MemberInfoDto memberInfoDto){
+        Member member = memberService.findMemberByEmail(memberInfoDto.getEmail());
+
+        return accountService.allAccount(member.getPrivateId())
+                .collectList() // Flux를 Mono<List<Object>>로 변환합니다.
+                .map(result -> new ResponseEntity<>(result, HttpStatus.OK))
+                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND)); // 만약 데이터가 없을 경우의 처리
     }
+
 }
---
+++ backend/src/main/java/com/easyone/travelance/domain/account/service/AccountService.java
@@ -0,0 +1,39 @@
+package com.easyone.travelance.domain.account.service;
+
+import com.easyone.travelance.domain.account.dto.AllAccountRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AccountService {
+
+    @Value("${http.auth-token-header.name}")
+    private String authTokenHeaderName;
+
+    @Value("${http.auth-token}")
+    private String authToken;
+
+    @Autowired
+    private final WebClient.Builder webClientBuilder;
+
+    public Flux<Object> allAccount(String privateId) {
+        return webClientBuilder.baseUrl("http://localhost:8081") // API 엔드포인트를 설정합니다.
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(authTokenHeaderName, authToken)
+                .build()
+                .post()
+                .uri("/bank/account/search/account")
+                .bodyValue(new AllAccountRequestDto(privateId)) // 요청 바디에 데이터를 설정합니다.
+                .retrieve()
+                .bodyToFlux(Object.class); // 응답을 Flux<Object> 형태로 받습니다.
+    }
+}
+
