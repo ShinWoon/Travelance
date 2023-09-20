a0116fe836209e1a5a68518c06f0ce7478d66e3c
DESKTOP-DOGVPUB\SSAFY <tjsgh1512@naver.com>
2023-09-20 18:01:25 +0900

    ✏ modify : LoginScreen UI 수정 | 차선호

--- Travelance/presentation/src/main/java/com/moneyminions/presentation/screen/login/LoginScreen.kt
+++ Travelance/presentation/src/main/java/com/moneyminions/presentation/screen/login/LoginScreen.kt
@@ -1,13 +1,18 @@
 package com.moneyminions.presentation.screen.login
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -22,17 +27,23 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.moneyminions.presentation.R
 import com.moneyminions.presentation.common.CustomTextStyle
+import com.moneyminions.presentation.screen.login.view.KakaoLoginButtonComponent
 import com.moneyminions.presentation.theme.KakaoLabelColor
 import com.moneyminions.presentation.theme.KakaoYellow
+import com.moneyminions.presentation.theme.PinkLight
+import com.moneyminions.presentation.theme.PinkMiddle
 import com.moneyminions.presentation.theme.White
 import com.moneyminions.presentation.viewmodel.login.LoginViewModel
 import kotlinx.coroutines.launch
@@ -42,12 +53,9 @@ import kotlinx.coroutines.launch
 @Composable
 fun LoginScreen(
     navController: NavHostController,
-    loginViewModel: LoginViewModel = hiltViewModel(),
     modifier: Modifier = Modifier
 ) {
-    val coroutineScope = rememberCoroutineScope()
     val snackbarHostState = remember { SnackbarHostState() }
-    val context = LocalContext.current
     Scaffold(
         modifier = modifier.fillMaxSize(),
         snackbarHost = { SnackbarHost(snackbarHostState) },
@@ -63,40 +71,27 @@ fun LoginScreen(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center,
             ) {
-                Button(
-                    modifier = modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 32.dp)
-                        .align(Alignment.Start),
-                    colors = ButtonDefaults.buttonColors(KakaoYellow),
-                    shape = RoundedCornerShape(6.dp),
-                    onClick = {
-                        loginViewModel.singInKakao(context)
-//                        coroutineScope.launch {
-//                            snackbarHostState.showSnackbar("로그인 되었습니다.")
-//                        }
-                    },
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(PinkLight),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Box(
-                        modifier = modifier.fillMaxWidth(),
-                        contentAlignment = Alignment.CenterStart,
-                    ) {
-                        Icon(
-                            modifier = modifier.padding(vertical = 4.dp),
-                            painter = painterResource(id = R.drawable.ic_kakao_logo),
-                            contentDescription = "kakao logo",
-                            tint = KakaoLabelColor,
-                        )
-                        Text(
-                            text = "카카오로 시작하기",
-                            color = KakaoLabelColor,
-                            style = CustomTextStyle.pretendardSemiBold16,
-                            modifier = modifier.fillMaxWidth(),
-                            textAlign = TextAlign.Center,
-                        )
-                    }
+                    Image(
+                        painter = painterResource(id = R.drawable.image_logo),
+                        contentDescription = "main_logo",
+                        modifier = Modifier.fillMaxSize(0.4f)
+                    )
+                    KakaoLoginButtonComponent()
                 }
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview(){
+    LoginScreen(navController = rememberNavController())
 }
