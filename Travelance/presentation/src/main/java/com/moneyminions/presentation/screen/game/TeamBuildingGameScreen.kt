79ee518ea4550eafaafb76babc319f5dcfb91608
황신운 <brandy.hwang@gmail.com>
2023-09-21 18:00:36 +0900

    ✨ feat : 팀 정하기 수정 | 황신운

--- Travelance/presentation/src/main/java/com/moneyminions/presentation/screen/game/TeamBuildingGameScreen.kt
+++ Travelance/presentation/src/main/java/com/moneyminions/presentation/screen/game/TeamBuildingGameScreen.kt
@@ -1,20 +1,37 @@
 package com.moneyminions.presentation.screen.game
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.moneyminions.presentation.common.TopBar
+import com.moneyminions.presentation.viewmodel.game.ChooseTeamGameViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TeamBuildingGameScreen(
-    navController: NavHostController
+    navController: NavHostController,
+    teamChooseTeamGameViewModel: ChooseTeamGameViewModel = hiltViewModel(),
 ) {
+    val teammateInfoList =
+        LaunchedEffect(true) {
+            teamChooseTeamGameViewModel.getTeammateInfo()
+        }
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
@@ -24,9 +41,31 @@ fun TeamBuildingGameScreen(
         Surface(
             modifier = Modifier
                 .padding(it)
-                .fillMaxSize()
+                .fillMaxSize(),
         ) {
-        
+        }
+    }
+}
+
+@Composable
+fun TeamCountView(
+    modifier: Modifier = Modifier,
+    teamChooseTeamGameViewModel: ChooseTeamGameViewModel
+) {
+    val teamCnt = teamChooseTeamGameViewModel.teamCnt
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+
+        }
+        Spacer(modifier = modifier.width(8.dp))
+        Text(text = )
+        Spacer(modifier = modifier.width(8.dp))
+        Button(onClick = { /*TODO*/ }) {
+
         }
     }
 }
