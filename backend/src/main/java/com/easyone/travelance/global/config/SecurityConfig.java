af726e4cc5e3786f7f6453eed9af1ec22f31e459
김제준 <hoilday5303@naver.com>
2023-09-21 15:23:17 +0900

    ⚙ config : 시큐리티 수정 | 김제준 |

--- backend/src/main/java/com/easyone/travelance/global/config/SecurityConfig.java
+++ backend/src/main/java/com/easyone/travelance/global/config/SecurityConfig.java
@@ -8,6 +8,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @RequiredArgsConstructor
 @Configuration
@@ -34,5 +39,18 @@ public class SecurityConfig {
     public TokenManager tokenManager() {
         return new TokenManager(accessTokenExpirationPeriod, refreshTokenExpirationPeriod, secretKey);
 
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://ec2-3-34-155-173.ap-northeast-2.compute.amazonaws.com:8080", "http://j9d210.p.ssafy.io:8081")); // 허용할 원본 도메인
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE")); // 허용할 HTTP 메서드
+        configuration.setAllowCredentials(false); // 쿠키 등 자격 증명과 함께 요청을 할 수 있도록 허용
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type")); // 허용할 헤더
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 위에서 정의한 CORS 설정 적용
+        return source;
     }
 }
