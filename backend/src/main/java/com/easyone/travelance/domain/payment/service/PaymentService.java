6bbd2a29a4f35b6aaff4432d333016b694edd8a0
혜진 <vvloveletter@naver.com>
2023-09-21 10:14:26 +0900

    🛠 fix : 오류수정 완료 | 조혜진
    kafka 관련 테스트 불가

--- backend/src/main/java/com/easyone/travelance/domain/payment/service/PaymentService.java
+++ backend/src/main/java/com/easyone/travelance/domain/payment/service/PaymentService.java
@@ -1,14 +1,14 @@
-//package com.easyone.travelance.domain.payment.service;
-//
-//import com.easyone.travelance.domain.payment.dto.CompleteCalculationRequestDto;
-//import com.easyone.travelance.domain.payment.dto.PushAlertRequestDto;
-//import com.easyone.travelance.domain.payment.dto.RegisterCashRequestDto;
-//import com.easyone.travelance.domain.payment.dto.TransferAccountRequestDto;
-//
-//public interface PaymentService {
-//
-//    String registerCash(RegisterCashRequestDto registerCashRequestDto);
-//    String pushAlertData(PushAlertRequestDto pushAlertRequestDto);
-//    String completeCalculation(CompleteCalculationRequestDto completeCalculationRequestDto);
-//    String transferAccount(TransferAccountRequestDto transferAccountRequestDto);
-//}
+package com.easyone.travelance.domain.payment.service;
+
+import com.easyone.travelance.domain.payment.dto.CompleteCalculationRequestDto;
+import com.easyone.travelance.domain.payment.dto.PushAlertRequestDto;
+import com.easyone.travelance.domain.payment.dto.RegisterCashRequestDto;
+import com.easyone.travelance.domain.payment.dto.TransferAccountRequestDto;
+
+public interface PaymentService {
+
+    String registerCash(RegisterCashRequestDto registerCashRequestDto);
+    String pushAlertData(PushAlertRequestDto pushAlertRequestDto);
+    String completeCalculation(CompleteCalculationRequestDto completeCalculationRequestDto);
+    String transferAccount(TransferAccountRequestDto transferAccountRequestDto);
+}
