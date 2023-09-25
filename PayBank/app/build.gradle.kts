5a9de0d47da2b8b9c4376b2959cf5e8a7da40b3b
theo-no <tjsgh1512@naver.com>
2023-09-25 23:45:22 +0900

    ✨ feat : PayBank에 permission 다는 중 | 차선호

--- PayBank/app/build.gradle.kts
+++ PayBank/app/build.gradle.kts
@@ -92,4 +92,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.9.0")
+
+    //permission
+    implementation("com.google.accompanist:accompanist-permissions:0.30.1")
 }
