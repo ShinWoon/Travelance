56385abc308c2fb41e8df14897b730fc9e3aadb1
혜진 <vvloveletter@naver.com>
2023-09-26 14:56:06 +0900

    🛠 fix : controller @ReqeustBody 수정 | 조혜진

--- backend/src/main/java/com/easyone/travelance/domain/payment/controller/PaymentController.java
+++ backend/src/main/java/com/easyone/travelance/domain/payment/controller/PaymentController.java
@@ -10,10 +10,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityNotFoundException;
 
@@ -26,7 +23,7 @@ public class PaymentController {
 
     @PostMapping("/cash")
     @Operation(summary = "현금 사용내역 등록")
-    public ResponseEntity<ResultDto> registerCash(RegisterCashRequestDto registerCashRequestDto) {
+    public ResponseEntity<ResultDto> registerCash(@RequestBody RegisterCashRequestDto registerCashRequestDto) {
         String response = paymentService.registerCash(registerCashRequestDto);
         ResultDto resultDto = new ResultDto(response);
         return new ResponseEntity<>(resultDto, HttpStatus.OK);
@@ -34,7 +31,7 @@ public class PaymentController {
 
     @PostMapping("/push/alert")
     @Operation(summary = "푸시알림 공금 유무 선택 후 데이터 서버로 전송", description = "공금일 때에만 boolean값을 true로 변경 후 API 요청")
-    public ResponseEntity<ResultDto> pushAlertData(PushAlertRequestDto pushAlertRequestDto) {
+    public ResponseEntity<ResultDto> pushAlertData(@RequestBody PushAlertRequestDto pushAlertRequestDto) {
         String response = paymentService.pushAlertData(pushAlertRequestDto);
         ResultDto resultDto = new ResultDto(response);
         return new ResponseEntity<>(resultDto, HttpStatus.OK);
@@ -42,7 +39,7 @@ public class PaymentController {
 
     @PostMapping("/complete")
     @Operation(summary = "정산완료 확인", description = "여행 방에 있는 인원 모두가 정산완료를 누르면 push 알림 전송")
-    public ResponseEntity<ResultDto> completeCalculation(CompleteCalculationRequestDto completeCalculationRequestDto) {
+    public ResponseEntity<ResultDto> completeCalculation(@RequestBody CompleteCalculationRequestDto completeCalculationRequestDto) {
         String response = paymentService.completeCalculation(completeCalculationRequestDto);
         ResultDto resultDto = new ResultDto(response);
         return new ResponseEntity<>(resultDto, HttpStatus.OK);
@@ -50,7 +47,7 @@ public class PaymentController {
 
     @PostMapping("/transfer")
     @Operation(summary = "계좌이체요청")
-    public ResponseEntity<ResultDto> transferAccount(TransferAccountRequestDto transferAccountRequestDto) {
+    public ResponseEntity<ResultDto> transferAccount(@RequestBody TransferAccountRequestDto transferAccountRequestDto) {
         String response = paymentService.transferAccount(transferAccountRequestDto);
         ResultDto resultDto = new ResultDto(response);
         return new ResponseEntity<>(resultDto, HttpStatus.OK);
