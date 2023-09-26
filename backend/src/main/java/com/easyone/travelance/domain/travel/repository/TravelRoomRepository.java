70451637268439ce9399b64a2d316b07b58eb2d5
혜진 <vvloveletter@naver.com>
2023-09-26 21:36:43 +0900

    ✨ feat : 여행방 결제내역 중 공금 및 개인 사용내역 조회 구현 | 조혜진

--- backend/src/main/java/com/easyone/travelance/domain/travel/repository/TravelRoomRepository.java
+++ backend/src/main/java/com/easyone/travelance/domain/travel/repository/TravelRoomRepository.java
@@ -1,12 +1,15 @@
 package com.easyone.travelance.domain.travel.repository;
 
 
+import com.easyone.travelance.domain.member.entity.Member;
 import com.easyone.travelance.domain.travel.entity.TravelRoom;
+import com.easyone.travelance.domain.travel.enumclass.RoomType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -16,4 +19,8 @@ public interface TravelRoomRepository extends JpaRepository<TravelRoom, Long> {
     Optional<TravelRoom> findByIdAndMemberId(@Param("roomId") Long roomId, @Param("memberId") Long memberId);
 
     @Query("SELECT tr FROM TravelRoom tr JOIN tr.travelRoomMembers trm JOIN trm.member m WHERE m.email = :email AND tr.id = :id")
-    Optional<TravelRoom> findByIdAndMemberEmail(@Param("id") Long id, @Param("email") String email);}
+    Optional<TravelRoom> findByIdAndMemberEmail(@Param("id") Long id, @Param("email") String email);
+
+    List<TravelRoom> findAllByTravelRoomMembers_MemberAndIsDone(Member member, RoomType roomType);
+
+}
