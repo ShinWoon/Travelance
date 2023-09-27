f2436eef95e8b1f7cd2bca595c97df1212274856
surimjeon <gds0813@naver.com>
2023-09-27 22:48:50 +0900

    🛠 fix : AddUser test2 | 전수림

--- backend/src/main/java/com/easyone/travelance/domain/travel/controller/TravelController.java
+++ backend/src/main/java/com/easyone/travelance/domain/travel/controller/TravelController.java
@@ -44,11 +44,12 @@ public class TravelController {
         return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
     }
 
-    // 프로필 설정, 내 방에 맞는 프로필을 저장함
+    // 프로필 설정, 내 방에 맞는 프로필을 저장함(build test)
     @Operation(summary = "내 프로필 설정하기", description = "방에 입장하고 싶은 맴버 정보와 프로필 사진을 요청하면, 여행참가자가 되며 유저리스트를 전달합니다. ")
     @PostMapping(value = "/{roomId}/addUser", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity <ResultDto> AddUser(@MemberInfo MemberInfoDto memberInfo, @RequestPart(value = "imageFiles") MultipartFile profileUrl, @PathVariable Long roomId) {
         Member member = memberService.findMemberByEmail(memberInfo.getEmail());
+        log.info(String.valueOf(profileUrl));
         RoomUserRequestDto roomUserRequestDto=new RoomUserRequestDto("닉네임");
         ResultDto resultDto =travelService.adduser(roomId, member, roomUserRequestDto, profileUrl);
         return new ResponseEntity<>(resultDto, HttpStatus.OK);
