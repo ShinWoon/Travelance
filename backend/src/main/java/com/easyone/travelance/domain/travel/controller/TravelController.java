cf525fab5eb899120ebe4580a596bbb476546023
surimjeon <gds0813@naver.com>
2023-09-28 20:39:02 +0900

    🛠 fix : 사진 제외 방만들기 fix | 전수림

--- backend/src/main/java/com/easyone/travelance/domain/travel/controller/TravelController.java
+++ backend/src/main/java/com/easyone/travelance/domain/travel/controller/TravelController.java
@@ -38,7 +38,8 @@ public class TravelController {
     @PostMapping(value = "")
     public ResponseEntity<RoomIdResponseDto> MakeRoom(@MemberInfo MemberInfoDto memberInfo,
                                                       @RequestPart RoomInfoRequestDto roomInfoRequestDto,
-                                                    @RequestPart RoomUserRequestDto roomUserRequestDto) {
+                                                      @RequestPart(value = "imageFiles", required = false) MultipartFile imageFiles,
+                                                      @RequestPart RoomUserRequestDto roomUserRequestDto) {
 
 //        System.out.println(imageFiles.isEmpty());
         Member member = memberService.findMemberByEmail(memberInfo.getEmail());
