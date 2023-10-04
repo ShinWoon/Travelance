fa0cdcb85e17886789b80acdb8703e19fd42b8ff
surimjeon <gds0813@naver.com>
2023-10-04 15:49:42 +0900

    🛠 fix : map 공금내역으로 수정  | 전수림

--- backend/src/main/java/com/easyone/travelance/domain/travel/service/MapService.java
+++ backend/src/main/java/com/easyone/travelance/domain/travel/service/MapService.java
@@ -43,7 +43,9 @@ public class MapService {
 //            }
 
             // 모든 결제내역의 위치장소를 리스트로 반환
-            List<Payment> paymentResponseDtoList = paymentRepository.findByTravelRoomId(roomId);
+//            List<Payment> paymentResponseDtoList = paymentRepository.findByTravelRoomId(roomId);
+
+            List<Payment> paymentResponseDtoList = paymentRepository.findByTravelRoomIdAndIsWithPaidIsTrue(roomId);
 
             //결제장소는 중복되지 않음
             Set<MapAllResponseDto> paymentLocation = new HashSet<>();
@@ -64,7 +66,7 @@ public class MapService {
     public List<MapDetailResponseDto> mapDetail(Long roomId, MapDetailRequestDto mapDetailRequestDto) {
         //dto에서 map의 주소를 가져와서 이 여행방의 이 장소에서 결제된 내역을 모두 가져와서 반환한다.
 
-        List<Payment> paymentList = paymentRepository.findByTravelRoomIdAndStoreAddress(roomId, mapDetailRequestDto.getStoreAddress());
+        List<Payment> paymentList = paymentRepository.findByTravelRoomIdAndStoreAddressAndIsWithPaidIsTrue(roomId, mapDetailRequestDto.getStoreAddress());
 
         List<MapDetailResponseDto> mapDetailResponseDtoList = new ArrayList<>();
 
